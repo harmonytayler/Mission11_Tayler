823d1225a172a66f786d5585a138608a7a9d17b0
Harmony Tayler <153692183+harmonytayler@users.noreply.github.com>
2025-04-02 11:20:30 -0600

    Getting ready to commit

--- backend/Mission11.API/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
+++ backend/Mission11.API/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c97feb9b309fb541965bd2ec5f9484fa52106cf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d456bb503da9832052ab3840ae7a85ddcd64b78d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
